256eb2f174cd159a3e098e6cf8805ff6f5dc8c6e
oksana12345678 <oksana.tanashuk@gmail.com>
2024-02-15 23:46:02 +0100

    add

---
+++ src/js/2-form.js
@@ -0,0 +1,43 @@
+const form = document.querySelector('.feedback-form');
+
+const saveForm = () => {
+  const formData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+const loadForm = () => {
+  const saveData = localStorage.getItem('feedback-form-state');
+  if (saveData) {
+    const { email, message } = JSON.parse(saveData);
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+  }
+};
+
+form.addEventListener('input', saveForm);
+
+window.addEventListener('load', loadForm);
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  if (form.elements.email.value.trim() === '') {
+    alert('Please enter your email');
+    return;
+  }
+
+  if (form.elements.message.value.trim() === '') {
+    alert('Please enter your message');
+    return;
+  }
+  localStorage.removeItem('feedback-form-state');
+
+  console.log({
+    email: form.elements.email.value.trim(),
+    message: form.elements.message.value.trim(),
+  });
+
+  form.reset();
+});
